c0b73f718657d76c99a3c5063efeab05e1785d42
mich2k <miky3848@gmail.com>
2020-05-06 02:29:53 +0200

    first working commit

--- hash_converter.py
+++ hash_converter.py
@@ -13,7 +13,7 @@ def hash_line_convert(line, selected_hash):
     elif(selected_hash == 3):
         return hashlib.md5(line.encode("utf-8")).hexdigest()
     else:
-        raise Exception('Unknown Identifier')
+        raise Exception('Unknown Hash Identifier')
 
 
 def inputf(filename):
@@ -40,30 +40,46 @@ def indention_select():
             Choose which indention format:
                 0 -\thash
                 1 -\trow_id | hash
-                2 -\trow_id | word | hash
-                3 -\tword | hash
+                2 -\tword | hash
+                3 -\trow_id | word | hash
                 
                 Your choice:   """)
 
 
-def hash_print(noext_filename, filename, selected_hash, ext):
-    hash_count = 0
+def hash_format(hash_count, selected_indention, line, curr_hash):
+    if(selected_indention == 0):
+        return curr_hash
+    elif(selected_indention == 1):
+        return ("%i - %s\n") % (hash_count, curr_hash)
+    elif(selected_indention == 2):
+        return ("%s - %s\n") % (line.rstrip(), curr_hash)
+    elif(selected_indention == 3):
+        return ("%i - %s - %s\n") % (hash_count, line.rstrip(), curr_hash)
+    else:
+        raise Exception('Unknown Indention Identifier')
+
+
+def hash_print(noext_filename, filename, selected_hash, ext, selected_indention):
+    hash_count = 1
     f_out = open(str(noext_filename) + "-hashed" + ext, 'w')
     with open(filename, 'r') as f_in:
         for line in f_in:
             curr_hash = hash_line_convert(line, selected_hash)
-            f_out.write(str(curr_hash) + '\n')
+            #f_out.write(str(curr_hash) + '\n')
+            print(hash_format(hash_count,selected_indention, line, curr_hash))
             hash_count += 1
-            #print("%s - %s\n" % (line.rstrip(),curr_hash ), end='')
     print(str(hash_count) + " have been converted succesfully")
     f_in.close()
     f_out.close()
 
+
 def main():
     selected_hash = int(hash_select())
+    selected_indention = int(indention_select())
     filename = input("Type here your file name: ")
     noext_filename, ext = inputf(filename)
-    hash_print(noext_filename, filename, selected_hash, ext)
+    hash_print(noext_filename, filename, 
+selected_hash, ext, selected_indention)
 
 
 if (__name__ == '__main__'):
